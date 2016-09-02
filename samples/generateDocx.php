5b82ecf2fb8552e272d27bb878653b5aedb28521
cheneyveron <cheneyveron@live.cn>
2016-09-02 22:37:43 +0800

    随机数长度增加为16位

--- samples/generateDocx.php
+++ samples/generateDocx.php
@@ -5,7 +5,7 @@ function RandomString()
 {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $randstring = '';
-    for ($i = 0; $i < 10; $i++) {
+    for ($i = 0; $i < 16; $i++) {
         $randstring .= $characters[rand(0, strlen($characters))];
     }
     return $randstring;
